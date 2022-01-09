e95ecb729b8acdedcec6a6befe59f89ba4ade7bd
KNebz <bnknebeker@gmail.com>
2022-01-08 22:58:16 -0500

    Finalize Content

---
+++ js/currentdate.js
@@ -0,0 +1,36 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d= new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+var h3 = document.querySelector("h3");
+var h4 = document.querySelector("h4");
+
+h3.textContent = "©" + year + "   Kelly A. Nebeker"
+h4.textContent = document.lastModified
