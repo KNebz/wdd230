67ebd890ca0c87d9b5124f22616d7ba757f06165
KNebz <bnknebeker@gmail.com>
2022-11-17 21:52:21 -0500

    added weather api

---
+++ weather-api/js/weatherapi.js
@@ -0,0 +1,22 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = "//api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=d07b07d2266cbb3af1ad3a654ea37a12&units=imperial";
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
