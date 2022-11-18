4bfe162c104c70554fa16e3137af57c294c58093
KNebz <bnknebeker@gmail.com>
2022-11-17 21:41:48 -0500

    Updated for zip code

--- chamber/weatherAPI for Christmas.js
+++ chamber/weatherAPI for Christmas.js
@@ -1,5 +1,5 @@
 //ADD the key and change units to imperial
-const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=d07b07d2266cbb3af1ad3a654ea37a12&units=imperial"
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=4150880&appid=d07b07d2266cbb3af1ad3a654ea37a12&units=imperial"
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
