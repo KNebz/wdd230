bf662f832aa21c679af2a3bd5895507ea2c357cb
KNebz <bnknebeker@gmail.com>
2022-11-19 09:29:05 -0500

    updated windchill.js

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -19,27 +19,28 @@ fetch(apiURL)
     document.getElementById('weather_icon').src = icon_path;
     document.getElementById('weather').innerHTML = `<strong>${weatherInfo.weather[0].description}</strong>`;
 
-}); //end of "then" fat arrow function
+    const input1 = document.querySelector('#currentTemp');
+    const input2 = document.querySelector('#windSpeed1');
+    const windchill = document.querySelector('#windchill1');
 
-const input1 = document.querySelector('#currentTemp');
-const input2 = document.querySelector('#windSpeed1');
-const windchill = document.querySelector('#windchill1');
+    console.log(input1.textContent);
 
-console.log(input1.textContent);
+    var temp = parseFloat(input1.textContent);
+    var windspeed = parseFloat(input2.textContent);
 
-var temp = parseFloat(input1.textContent);
-var windspeed = parseFloat(input2.textContent);
+    console.log(temp);
+    console.log(windspeed);
 
-console.log(temp);
-console.log(windspeed);
+    wcc = 35.74 + 0.6215*temp - 35.75*(windspeed**0.16) + 0.4275*temp*(windspeed**0.16);
 
-wcc = 35.74 + 0.6215*temp - 35.75*(windspeed**0.16) + 0.4275*temp*(windspeed**0.16);
+    wccalc = Math.round(wcc);
 
-wccalc = Math.round(wcc);
-
-if (temp <= 50 &&
-    windspeed > 3) {
+    if (temp <= 50 &&
+        windspeed > 3) {
         windchill.textContent = wccalc
     } else {
         windchill.textContent = "N/A"
     }
+
+}); //end of "then" fat arrow function
+
