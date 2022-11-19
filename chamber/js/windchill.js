cb48bbd0c1b8827ddc55ac2c9ddf0573817c7bfc
KNebz <bnknebeker@gmail.com>
2022-11-19 09:12:09 -0500

    Added wearher API

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,5 +1,28 @@
-const input1 = document.querySelector('#temp1');
-const input2 = document.querySelector('#windspeed1');
+const apiURL = "//api.openweathermap.org/data/2.5/weather?zip=32709,us&appid=d07b07d2266cbb3af1ad3a654ea37a12&units=imperial"
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
+    document.getElementById('windSpeed1').innerHTML=weatherInfo.wind.speed;
+
+    const iconcode = weatherInfo.weather[0].icon;
+    console.log(iconcode);
+
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
+
+    document.getElementById('weather_icon').src = icon_path;
+    document.getElementById('weather').innerHTML = `<strong>${weatherInfo.weather[0].description}</strong>`;
+
+ }); //end of "then" fat arrow function
+
+const input1 = document.querySelector('#currentTemp');
+const input2 = document.querySelector('#windSpeed1');
 const windchill = document.querySelector('#windchill1');
 
 var temp = parseFloat(input1.textContent);
