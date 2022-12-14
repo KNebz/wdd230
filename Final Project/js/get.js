08f2f36dcc233b6e2f35451ed9cd104963ff9350
KNebz <bnknebeker@gmail.com>
2022-12-13 22:54:43 -0500

    Updated form javascript

--- Final Project/js/get.js
+++ Final Project/js/get.js
@@ -1,69 +1,49 @@
 
-const URL = 'https://knebz.github.io/wdd230/chamber/js/data.json';
-const display = document.getElementById("spots");
+const URL = 'https://knebz.github.io/wdd230/Final%20Project/js/fruit.json';
 
-function buildBusinessCards(info) {
-    let data = info.businesses.filter((p) => p.memberLvl == "Gold" || p.memberLvl == "Silver");
-    console.log(data);
-    i=0;
-    while (i<3) {
-      a=data.splice(Math.floor(Math.random() * data.length), 1);
-      i = i + 1;
-  
-};
+console.log(URL);
 
-console.log(a);
-console.log(data);  
+let dropdown1 = document.getElementById('fruit1_dropdown');
+let dropdown2 = document.getElementById('fruit2_dropdown');
+let dropdown3 = document.getElementById('fruit3_dropdown');
+let addOption = document.createElement("option");
 
+function makeDropDown (info) {
+  for (let i=0; i < info.length; i++) {
+    let fruits = info[i].name;
+    console.log(fruits);
+    option = document.createElement("option");
+    option.text = fruits;
+    option.value = fruits;
+    dropdown1.add(option);
+  }
+  for (let i=0; i < info.length; i++) {
+    let fruits = info[i].name;
+    console.log(fruits);
+    option = document.createElement("option");
+    option.text = fruits;
+    option.value = fruits;
+    dropdown2.add(option);
+  }
+  for (let i=0; i < info.length; i++) {
+    let fruits = info[i].name;
+    console.log(fruits);
+    option = document.createElement("option");
+    option.text = fruits;
+    option.value = fruits;
+    dropdown3.add(option);
+  }
 
-let num = 7;
-data.forEach((business) => {
-
-    // Create elements to add to the document
-  let card = document.createElement("div");  
-  let h3 = document.createElement('h3');
-  let address = document.createElement('p');
-  let phoneNumber = document.createElement('p');
-  let website = document.createElement('a');
-  let portrait = document.createElement('img');
-
-
-    // Change the textContent property of the h2 element to contain the prophet's full name
-  card.setAttribute("class", `box${num}`);  
-  h3.textContent = `${business.name}`;
-  address.textContent = `${business.address}`;
-  phoneNumber.textContent = `${business.phoneNumber}`;
-  website.textContent = "Visit Our Website"
-
-  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  website.setAttribute('href', business.website)
-  portrait.setAttribute('src', business.imgLoc);
-  portrait.setAttribute('alt', `Icon for ${business.name}`);
-  portrait.setAttribute('loading', 'lazy');
-
-  // Add/append the section(card) with the h2 element
-  card.appendChild(portrait);
-  card.appendChild(h3);
-  card.appendChild(address);
-  card.appendChild(phoneNumber);
-  card.appendChild(website)
- 
-
-  // Add/append the existing HTML div with the cards class with the section(card)
-
-  display.append(card);
-  num += 1;
- });
 }
 
-async function getBusinesses() {
-    let response = await fetch(URL);
-    if (response.ok) {
-      let data = await response.json();
-      buildBusinessCards(data);
-    } else {
-      throw Error(response.statusText);
-    }
+async function pullData() {
+  let response = await fetch(URL);
+  if (response.ok) {
+    let data = await response.json();
+    makeDropDown (data);
+  } else {
+    throw Error(response.statusText);
   }
-  
-  getBusinesses();
+}
+
+pullData();
